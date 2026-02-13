6dfe46f278950b6ca352bf4c4f1c9945ae9a009e
Charvi00 <charviaggarwal2005@gmail.com>
2026-02-13 22:41:38 +0530

    Train Duration Prediction Web App

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load model
+model = joblib.load("trainduration_model.pkl")
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    prediction = None
+
+    if request.method == "POST":
+        distance = float(request.form["distance"])
+        stops = int(request.form["stops"])
+
+        data = np.array([[distance, stops]])
+        result = model.predict(data)[0]
+
+        prediction = round(result, 2)
+
+    return render_template("index.html", prediction=prediction)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
